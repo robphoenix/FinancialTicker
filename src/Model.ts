96f38e97d0c7980143564cef0dbf44ddedd23f1a
Rob Phoenix <rob@robphoenix.com>
2018-06-23 09:06:05 +0100

    Add Stock class

--- src/Model.ts
+++ src/Model.ts
@@ -1,13 +1,31 @@
 class Model {
-    constructor() {}
+    public headers: string[];
+    public stocks: IStock[];
+
+    constructor() {
+        this.headers = [];
+        this.stocks = [];
+    }
 
     public load = (filePath: string) => {
         fetch(filePath)
             .then((res) => res.text())
             .then((text) => this.parseCSV(text));
-    }
+    };
 
     private parseCSV = (text: string) => {
-        console.log(text);
-    }
+        const [headers, ...stocks] = text.split("\n");
+        this.headers = headers.split(",");
+        this.stocks = stocks.filter((stock) => stock !== "").map((stock) => {
+            const [name, companyName, price, change, changePercent, marketCap] = stock.split(",");
+            return new Stock({
+                change: +change,
+                changePercent,
+                companyName,
+                marketCap,
+                name,
+                price: +price,
+            });
+        });
+    };
 }
---
+++ src/Stock.ts
@@ -0,0 +1,26 @@
+interface IStock {
+    name: string;
+    companyName: string;
+    price: number;
+    change: number;
+    changePercent: string;
+    marketCap: string;
+}
+
+class Stock implements IStock {
+    public name: string;
+    public companyName: string;
+    public price: number;
+    public change: number;
+    public changePercent: string;
+    public marketCap: string;
+
+    constructor(data: IStock) {
+        this.name = data.name;
+        this.companyName = data.companyName;
+        this.price = data.price;
+        this.change = data.change;
+        this.changePercent = data.changePercent;
+        this.marketCap = data.marketCap;
+    }
+}
