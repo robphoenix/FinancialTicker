b3351c2ba4297e9bceb441ae4016ffd5b7d673b7
Rob Phoenix <rob@robphoenix.com>
2018-06-25 21:47:23 +0100

    Add CSS

--- src/Grid.ts
+++ src/Grid.ts
@@ -17,15 +17,25 @@ class Grid {
         const rowHead: HTMLTableRowElement = thead.insertRow(0);
         model.headers.map((header, i) => {
             const cell: HTMLTableDataCellElement = rowHead.insertCell(i);
+            cell.className = "grid__thead";
             cell.innerHTML = header;
         });
 
         const tbody: HTMLTableSectionElement = table.createTBody();
+        tbody.className = "grid__tbody";
         model.stocks.map((stock, i) => {
             const rowBody: HTMLTableRowElement = tbody.insertRow(i);
+            if (i % 2 === 0) {
+                rowBody.className = "grid__row--even ";
+            }
+            rowBody.className += "grid__row";
             Object.keys(stock).map((key, j) => {
                 const cell: HTMLTableDataCellElement = rowBody.insertCell(j);
                 cell.innerHTML = stock[key];
+                if (key === "name") {
+                    cell.className += "grid__cell--name ";
+                }
+                cell.className += "grid__cell";
             });
         });
     }
